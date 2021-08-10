fd93f4be920d5893394877c1789c412844227921
Jelani Woods <jelani@firstdraft.com>
2021-08-09 21:44:22 -0500

    Remove constraints

--- db/migrate/20210809170533_add_director_reference_to_movies.rb
+++ db/migrate/20210809170533_add_director_reference_to_movies.rb
@@ -2,6 +2,5 @@ class AddDirectorReferenceToMovies < ActiveRecord::Migration[6.0]
   def change
     add_foreign_key :movies, :directors
     add_index :movies, :director_id
-    change_column_null :movies, :director_id, false
   end
 end
--- db/migrate/20210809170540_add_movie_reference_to_characters.rb
+++ db/migrate/20210809170540_add_movie_reference_to_characters.rb
@@ -2,6 +2,5 @@ class AddMovieReferenceToCharacters < ActiveRecord::Migration[6.0]
   def change
     add_foreign_key :characters, :movies
     add_index :characters, :movie_id
-    change_column_null :characters, :movie_id, false
   end
 end
